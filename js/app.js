94b78f4e2b50e1625e6498aed44dbcb594891174
Alex <pickeringacw@gmail.com>
2018-02-21 18:07:23 +0200

    Added side nav. Implemented KO. Added observable search parameter

--- js/app.js
+++ js/app.js
@@ -1,113 +1,41 @@
 //https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&mode=bicycling&origins=4800+El+Camino+Road,+Los+Altos,+California&destinations=2465+Latham+Street,+Mountain+View,+California&key=AIzaSyBOSlRsVnEzPxDg6V8peMv6hzr4-GOkqtY
+function AppViewModel() {
+	let self = this;
 
-let locations = [
-	{
-		id: 1,
-		title: 'SAPS Kirstenhof',
-		location: {
-			lat: -34.072077,
-			lng: 18.4551723
-		}
-	},
-	{
-		id: 2,
-		title: 'Kirstenhof Primary School',
-		location: {
-			lat: -34.0696115,
-			lng: 18.4554241
-		}
-	},
-	{
-		id: 3,
-		title: 'Wise Owl Pre-school',
-		location: {
-			lat: -34.07289,
-			lng: 18.4489899
-		}
-	},
-	{
-		id: 4,
-		title: 'Hair Gallery',
-		location: {
-			lat: -34.065176,
-			lng: 18.4515428
-		}
-	},
-	{
-		id: 5,
-		title: 'The Church of the Holy Spirit',
-		location: {
-			lat: -34.0788875,
-			lng: 18.4545959
-		}
-	},
-	{
-		id: 6,
-		title: 'WESSA (Wildlife and Environment Society of South Africa)',
-		location: {
-			lat: -34.07408,
-			lng: 18.44941
-		}
-	},
-	{
-		id: 7,
-		title: 'Sound Solutions - Wedding DJ',
-		location: {
-			lat: -34.0714431,
-			lng: 18.4480019
-		}
-	},
-	{
-		id: 8,
-		title: 'Kirstenhof Car Hire',
-		location: {
-			lat: -34.075361,
-			lng: 18.457439
-		}
-	},
-	{
-		id: 9,
-		title: 'Domino\'s Pizza',
-		location: {
-			lat: -34.067027,
-			lng: 18.457631
-		}
-	},
-	{
-		id: 10,
-		title: 'Kirstenhof Dance & Pilates Studio',
-		location: {
-			lat: -34.0691599,
-			lng: 18.45323
-		}
-	}
-];
+	this.searchOption = ko.observable("");
 
-let markers = [];
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.072788, lng: 18.4536387},
-		zoom: 14
-	});
-
-	initMarkers();
-}
-
-function initMarkers() {
-	bounds = new google.maps.LatLngBounds();
-
-	locations.forEach((location) => {
-		let marker = new google.maps.Marker({
-			map: map,
-			position: location.location,
-			title: location.title,
-			id: location.id,
-			animation: google.maps.Animation.DROP
+	this.initMap = function() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: -34.072788, lng: 18.4536387},
+			zoom: 14,
+			disableDefaultUI: true
 		});
 
-		bounds.extend(marker.position);
+		this.initMarkers();
+	};
 
-		markers.push(marker);
-	});
+	this.initMarkers = function() {
+		locations.forEach((location) => {
+			new google.maps.Marker({
+				map: map,
+				position: location.location,
+				title: location.title,
+				id: location.id,
+				animation: google.maps.Animation.DROP
+			});
+		});
+	};
+
+	this.toggleNav = function() {
+		console.log(this.searchOption());
+		(document.getElementById("side-nav").style.width === "100%") ?
+			document.getElementById("side-nav").style.width = "0" :
+			document.getElementById("side-nav").style.width = "100%";
+	};
+
+	this.initMap();
+}
+
+function startApp() {
+	ko.applyBindings(new AppViewModel());
 }
---
+++ js/locations.js
@@ -0,0 +1,82 @@
+let locations = [
+	{
+		id: 1,
+		title: 'SAPS Kirstenhof',
+		location: {
+			lat: -34.072077,
+			lng: 18.4551723
+		}
+	},
+	{
+		id: 2,
+		title: 'Kirstenhof Primary School',
+		location: {
+			lat: -34.0696115,
+			lng: 18.4554241
+		}
+	},
+	{
+		id: 3,
+		title: 'Wise Owl Pre-school',
+		location: {
+			lat: -34.07289,
+			lng: 18.4489899
+		}
+	},
+	{
+		id: 4,
+		title: 'Hair Gallery',
+		location: {
+			lat: -34.065176,
+			lng: 18.4515428
+		}
+	},
+	{
+		id: 5,
+		title: 'The Church of the Holy Spirit',
+		location: {
+			lat: -34.0788875,
+			lng: 18.4545959
+		}
+	},
+	{
+		id: 6,
+		title: 'WESSA (Wildlife and Environment Society of South Africa)',
+		location: {
+			lat: -34.07408,
+			lng: 18.44941
+		}
+	},
+	{
+		id: 7,
+		title: 'Sound Solutions - Wedding DJ',
+		location: {
+			lat: -34.0714431,
+			lng: 18.4480019
+		}
+	},
+	{
+		id: 8,
+		title: 'Kirstenhof Car Hire',
+		location: {
+			lat: -34.075361,
+			lng: 18.457439
+		}
+	},
+	{
+		id: 9,
+		title: 'Domino\'s Pizza',
+		location: {
+			lat: -34.067027,
+			lng: 18.457631
+		}
+	},
+	{
+		id: 10,
+		title: 'Kirstenhof Dance & Pilates Studio',
+		location: {
+			lat: -34.0691599,
+			lng: 18.45323
+		}
+	}
+];
