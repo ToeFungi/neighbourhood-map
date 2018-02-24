19bc0ce6056cf442fd3a3f32b0c2a631070efaf7
Alex <pickeringacw@gmail.com>
2018-02-24 19:56:21 +0200

    Implement filtered markers
    Filter out markers according to searched text

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-let map, clientId, clientSecret;
+let map;
 
 function AppViewModel() {
-	this.searchOption = ko.observable("");
-	this.filteredLocations = ko.observable(locations);
-	this.markers = [];
-	this.infoWindow = new google.maps.InfoWindow();
+    this.searchOption = ko.observable("");
+    this.filteredLocations = ko.observable(locations);
+    this.markers = [];
+    this.infoWindow = new google.maps.InfoWindow();
 
-	this.initMap = () => {
-		this.map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -34.072788, lng: 18.4536387},
-			zoom: 14,
-			disableDefaultUI: true
-		});
+    this.initMap = () => {
+        this.map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.072788, lng: 18.4536387},
+            zoom: 14,
+            disableDefaultUI: true
+        });
 
-		this.initMarkers();
-	};
+        this.initMarkers();
+    };
 
     this.initMarkers = () => locations.forEach((location) => {
         let marker = new google.maps.Marker({
@@ -26,8 +26,8 @@ function AppViewModel() {
         });
 
         google.maps.event.addListener(marker, 'click', () => {
-        	this.openInfoWindow(marker);
-        	marker.setAnimation(google.maps.Animation.DROP);
+            this.openInfoWindow(marker);
+            marker.setAnimation(google.maps.Animation.DROP);
         });
 
         this.markers.push(marker);
@@ -35,13 +35,23 @@ function AppViewModel() {
 
 
     this.searchOption.subscribe((search) => {
-    	let searched = locations.filter((location) => {
-    		return location.title.toLowerCase().indexOf(search.toLowerCase()) > -1;
-		});
+        let searched = locations.filter((location) => {
+            return location.title.toLowerCase().indexOf(search.toLowerCase()) > -1;
+        });
         this.filteredLocations(searched);
+
+        this.markers.forEach((marker) => {
+            for(let i = 0; i < this.filteredLocations().length; i++) {
+                if(marker.id === this.filteredLocations()[i].id) {
+                    marker.setVisible(true);
+                    return true;
+                }
+            }
+            marker.setVisible(false);
+        });
     });
 
-	this.openInfoWindow = (marker) => {
+    this.openInfoWindow = (marker) => {
         if(this.infoWindow.marker && this.infoWindow.marker.id === marker.id) return;
 
         this.infoWindow.marker = marker;
@@ -51,23 +61,23 @@ function AppViewModel() {
         this.infoWindow.setContent(`<div>Loading data from Foursquare</div>`);
         this.infoWindow.addListener('closeclick', () => this.infoWindow.marker = null);
         this.infoWindow.open(map, marker);
-	};
+    };
 
-	this.getFoursquareData = () => {
-		this.clientId = 'Q44UCWZHNTBACXILOCW2VWKDZIEOFFFYJHE2PKHF4YGA21BD';
-		this.clientSecret = 'NL3QNYHNQABL0A4DVHQODX2JTSAKR4AIJCH4AEG5YKURRCDP';
+    this.getFoursquareData = () => {
+        let clientId = 'Q44UCWZHNTBACXILOCW2VWKDZIEOFFFYJHE2PKHF4YGA21BD';
+        let clientSecret = 'NL3QNYHNQABL0A4DVHQODX2JTSAKR4AIJCH4AEG5YKURRCDP';
 
-		let marker = this.infoWindow.marker;
-        let apiUrl = `https://api.foursquare.com/v2/venues/search?ll=${marker.position.lat()},${marker.position.lng()}&client_id=${this.clientId}&client_secret=${this.clientSecret}&query=${marker.title}&v=20180224&m=foursquare`;
+        let marker = this.infoWindow.marker;
+        let apiUrl = `https://api.foursquare.com/v2/venues/search?ll=${marker.position.lat()},${marker.position.lng()}&client_id=${clientId}&client_secret=${clientSecret}&query=${marker.title}&v=20180224&m=foursquare`;
 
         fetch(apiUrl, {
-        	method: 'get'
-		}).then((resp) => {
-			return resp.json();
+            method: 'get'
+        }).then((resp) => {
+            return resp.json();
         }).then((evt) => {
             this.infoWindow.setContent(`<h4>${this.infoWindow.marker.title}</h4><p>${this.infoWindow.marker.position.lat()}, ${this.infoWindow.marker.position.lng()}</p>`);
 
-        	if(evt.response.venues.length === 0) throw Error('Foursquare has no information of this place.');
+            if(evt.response.venues.length === 0) throw Error('Foursquare has no information of this place.');
 
             let venue = evt.response.venues.filter((venue) => {
                 return venue.name.toLowerCase() === this.infoWindow.marker.title.toLowerCase();
@@ -86,13 +96,13 @@ function AppViewModel() {
             obj.lat = venue.location.lat;
             obj.lng = venue.location.lng;
 
-        	this.updateInfoWindow(obj);
-		}).catch((err) => {
-			alert(err);
-		});
-	};
+            this.updateInfoWindow(obj);
+        }).catch((err) => {
+            alert(err);
+        });
+    };
 
-	this.updateInfoWindow = (obj) => {
+    this.updateInfoWindow = (obj) => {
         this.infoWindow.setContent(`
 			<h4>${obj.title}</h4>
 			<p>${obj.category}</p>
@@ -102,30 +112,30 @@ function AppViewModel() {
 			<p>${obj.country}</p>
 			<p>${obj.lat}, ${obj.lng}</p>
 			<p>Powered by Foursquare</p>`);
-	};
-
-	this.getMarkerData = (markerId) => {
-		return locations.filter((location) => {
-			return location.id === markerId;
-		})[0];
-	};
-
-    this.getInfo = (evt) => {
-    	this.toggleNav();
-    	let marker = this.markers.filter((marker) => {
-    		return marker.id === evt.id;
-		});
-    	this.openInfoWindow(marker[0]);
     };
 
-	this.toggleNav = () =>
-		(document.getElementById("side-nav").style.width === "100%") ?
-			document.getElementById("side-nav").style.width = "0" :
-			document.getElementById("side-nav").style.width = "100%";
+    this.getMarkerData = (markerId) => {
+        return locations.filter((location) => {
+            return location.id === markerId;
+        })[0];
+    };
 
-	this.initMap();
+    this.getInfo = (evt) => {
+        this.toggleNav();
+        let marker = this.markers.filter((marker) => {
+            return marker.id === evt.id;
+        });
+        this.openInfoWindow(marker[0]);
+    };
+
+    this.toggleNav = () =>
+        (document.getElementById("side-nav").style.width === "100%") ?
+            document.getElementById("side-nav").style.width = "0" :
+            document.getElementById("side-nav").style.width = "100%";
+
+    this.initMap();
 }
 
 function startApp() {
-	ko.applyBindings(new AppViewModel());
+    ko.applyBindings(new AppViewModel());
 }
